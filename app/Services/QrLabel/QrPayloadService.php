f9a71fd0782358e57be3d9713c0616a6c19ebfde
syad <irsyadysfb05@gmail.com>
2026-07-19 23:18:41 +0800

    feat: adjust QR code dimensions and error correction settings in payload generation

--- app/Services/QrLabel/QrPayloadService.php
+++ app/Services/QrLabel/QrPayloadService.php
@@ -146,7 +146,7 @@ class QrPayloadService
             'GAP 2 mm, 0 mm',
             'DIRECTION 1',
             'CLS',
-            "QRCODE 8,8,{$qrPrintSettings['ecc']},3,A,0,M2,S1,\"{$qrPrintSettings['payload']}\"",
+            "QRCODE 15,31,{$qrPrintSettings['ecc']},2,A,0,M2,S1,\"{$qrPrintSettings['payload']}\"",
             'TEXT 140,15,"0",0,1,1,"TREMED Surgical Solution"',
             'TEXT 140,31,"0",0,1,1,"No 6-1, Block A,"',
             'TEXT 140,47,"0",0,1,1,"Zenith Corporate Park,"',
